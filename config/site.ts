79ecdd07323a7d5426b233e8af44a021725c60b2
Vedran Mikulček <Vedran.Mikulcek@iskon.hr>
2023-09-19 22:00:09 +0200

    Added themes and main navbar

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+    name: "dive-deeper/ui",
+    url: "https://ui.dive-deeper.com",
+    ogImage: "https://ui.shadcn.com/og.jpg",
+    description:
+      "Beautifully designed getting stuff done system.",
+    links: {
+      twitter: "https://twitter.com/dive_deeper",
+      github: "https://github.com/dive-deeper",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
